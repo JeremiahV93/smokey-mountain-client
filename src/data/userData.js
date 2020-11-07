08f7e19216cf2109333cf385355368c7fb04c3bd
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-11-07 13:18:34 -0600

    installed axios and tested python server, data comes back

---
+++ src/data/userData.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const url = 'http://localhost:8088';
+
+// const getUserbyID = (id) => new Promise((resolve, reject) => {
+//   axios.get(`${url}/users/${id}`)
+//     .then((res) => resolve(res))
+//     .catch((err) => reject(err));
+// });
+
+const getUserbyID = (id) => axios.get(`${url}/users/${id}`);
+
+export default { getUserbyID };
