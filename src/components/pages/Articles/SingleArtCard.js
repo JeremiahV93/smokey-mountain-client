b9fbf051a5bd1170a35800aceca8ef0abadaddf3
Mark Young <markyaaronyoung@gmail.com>
2020-11-10 21:01:25 -0600

    created a button on single article card to direct a user to a single article

--- src/components/pages/Articles/SingleArtCard.js
+++ src/components/pages/Articles/SingleArtCard.js
@@ -8,7 +8,7 @@ class SingleArtCard extends React.Component {
             <h1>{art.title} </h1>
             <h2>{art.name} </h2>
             <h3>{art.category}</h3>
-            <button className='btn btn-success'> Articles </button>
+            <button className='btn btn-success'>Article</button>
         </div>
     );
   }
