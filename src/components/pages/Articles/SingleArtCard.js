24965be13b17b64ce511d5f16a9d084ff4da215a
Mark Young <markyaaronyoung@gmail.com>
2020-11-12 20:44:39 -0600

    added a delete button to Article card

--- src/components/pages/Articles/SingleArtCard.js
+++ src/components/pages/Articles/SingleArtCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import articleData from '../../../data/articleData';
 
 class SingleArtCard extends React.Component {
+  deleteArticle = (e) => {
+    e.preventDefault();
+    const { art } = this.props;
+    console.error(art.id);
+    articleData.deleteArticle(art.id)
+      .then(() => {
+        this.props.history.push('/articles');
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
     const { art } = this.props;
     return (
-        <div className='card'>
-            <h1>{art.title} </h1>
-            <h2>{art.name} </h2>
-            <h3>{art.category}</h3>
+        <div className='card single-art row justify-content-center'>
+            <h2 className='card-title'>{art.title} </h2>
+            <h2 className='card-name'>{art.name} </h2>
+            <h3 className='cart-category'>{art.category}</h3>
             <button className='btn btn-success'>Read Article</button>
+            <button className='btn btn-warning' onClick={this.deleteArticle}>Delete Category</button>
         </div>
     );
   }
