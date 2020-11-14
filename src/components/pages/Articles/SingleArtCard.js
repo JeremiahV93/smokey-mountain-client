55ac89d9fe9988a518c9eb7f1d92d6744c75b99b
Mark Young <markyaaronyoung@gmail.com>
2020-11-14 09:35:43 -0600

    article card now prints display name of article onto articles page

--- src/components/pages/Articles/SingleArtCard.js
+++ src/components/pages/Articles/SingleArtCard.js
@@ -4,9 +4,9 @@ import articleData from '../../../data/articleData';
 class SingleArtCard extends React.Component {
   deleteArticle = (e) => {
     e.preventDefault();
-    const { art } = this.props;
-    console.error(art.id);
-    articleData.deleteArticle(art.id)
+    const { article } = this.props;
+    console.error(article.id);
+    articleData.deleteArticle(article.id)
       .then(() => {
         this.props.history.push('/articles');
       })
@@ -14,12 +14,12 @@ class SingleArtCard extends React.Component {
   }
 
   render() {
-    const { art } = this.props;
+    const { article } = this.props;
     return (
         <div className='card single-art row justify-content-center'>
-            <h2 className='card-title'>{art.title} </h2>
-            <h2 className='card-name'>{art.name} </h2>
-            <h3 className='cart-category'>{art.category}</h3>
+            <h2 className='card-title'>{article.title} </h2>
+            <h2 className='card-name'>{article.user.display_name} </h2>
+            <h3 className='cart-category'>{article.category}</h3>
             <button className='btn btn-success'>Read Article</button>
             <button className='btn btn-warning' onClick={this.deleteArticle}>Delete Category</button>
         </div>
